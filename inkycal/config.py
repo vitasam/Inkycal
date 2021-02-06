ca30f5d4ebcc933946e00ee2ce1e9c63bad1c10e
Ace <saadnaseer63@yahoo.de>
2021-02-06 23:06:21 +0100

    Use config file for Inkycal environment variables
    It's now more transparent where which files are located, loaded from and saved. Also part of allowing Inkycal to be placed in any standard directory (relative imports).

---
+++ inkycal/config.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Configuration file for Inky-Calendar Project
+Handles variables for both the web-ui and Inkycal core
+Do not change unless you know what you are doing.
+
+Copyright by aceisace
+"""
+
+from os.path import abspath
+
+def directory_up(levels):
+  current_path = abspath(__file__)
+  return "/" + "/".join(current_path.split("/")[1:][:-levels]) + "/"
+
+config = {
+  # Logfile
+  "LOGGING_PATH": directory_up(2) + "logs/inkycal.log",
+
+  # External content
+  "FONT_DIR": directory_up(2) + "external/fonts/",
+  "IMAGE_DIR": directory_up(2) + "external/images/",
+  "ICAL_DIR" : directory_up(2) + "external/icalendars/",
+
+  # Entry-point for uploads or external content
+  "UPLOAD_PATH": directory_up(2)+"external/",
+
+  
+  # Path for temporary content
+  "TEMP_DIR": directory_up(2) + "images/",
+
+  # Path for E-Paper drivers
+  "DRIVER_DIR": directory_up(1) + "display/drivers/",
+
+
+  # Path for Inykcal run file
+  "INKYCAL_RUNFILE": directory_up(2)+"inkyrun.py",
+
+  # Settings path
+  "SETTINGS_PATH": directory_up(3)+"settings.json"
+  }
