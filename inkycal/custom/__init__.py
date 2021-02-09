a437019a5941134912ec9c5e9804ebf9fb38f691
Ace <saadnaseer63@yahoo.de>
2021-02-09 19:52:34 +0100

    Adapt for additional functions

--- inkycal/custom/__init__.py
+++ inkycal/custom/__init__.py
@@ -1,4 +1,5 @@
-from .functions import get_fonts, write, text_wrap, draw_border
+from .functions import write, text_wrap, draw_border
 from .get_system_tz import get_system_tz
 from .internet_available import internet_available
 from .auto_fontsize import auto_fontsize
+from .load_external import load_external
