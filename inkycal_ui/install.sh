09dcfdabd429f3bbec142af647ec66725ba37ac5
Ace <saadnaseer63@yahoo.de>
2021-02-09 18:52:11 +0100

    Use symbolic link for web-ui

--- inkycal_ui/install.sh
+++ inkycal_ui/install.sh
@@ -6,8 +6,8 @@ pip3 install -r requirements.txt
 # install apache2 and dependencies
 xargs -a packages.txt sudo apt-get install -y
 
-# move apache2 conf file to correct location
-sudo mv inkycal.conf /etc/apache2/sites-available
+# copy apache2 conf file to correct location
+sudo cp inkycal.conf /etc/apache2/sites-available/
 
 # disable currently active site
 sudo a2dissite 000-default.conf
@@ -15,9 +15,9 @@ sudo a2dissite 000-default.conf
 # enable inkycal site
 sudo a2ensite inkycal.conf
 
-# move web-ui at correct place
+# Add shortcut (symbolic link) of web-ui in /var/www
 cd ..
-mv /home/$USER/Inkycal/inkycal_ui /var/www
+sudo ln -s /home/$USER/Inkycal/inkycal_ui /var/www
 
 # update changes
 sudo systemctl restart apache2
