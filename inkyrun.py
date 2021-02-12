76db6918b040be60733fb4627f34af59c8cc8a59
Ace <saadnaseer63@yahoo.de>
2021-02-12 08:45:27 +0100

    Added inkyrun file for simply running inkycal

---
+++ inkyrun.py
@@ -0,0 +1,19 @@
+import arrow
+from inkycal import Inkycal
+from inkycal.config import config
+
+
+now = arrow.now()
+start = now.format('DD-MM-YYYY HH:mm:ss')
+print(f'Inkycal starting. Date and time: {start}\n')
+
+print('--------------loading settings file------------')
+inky = Inkycal(config["INKYCAL_RUNFILE"], render = True)
+print('--------------settings file loaded-------------')
+
+print('--------------testing inkycal------------------')
+inky.test()
+print('--------------test complete--------------------')
+
+print('--------------starting main program------------')
+inky.run()
